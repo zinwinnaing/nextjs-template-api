9ad709755b7b9083a440f3584d77e60e0be12018
Zin Win Naing (Achromex) <zinwinnaing@achromex.com>
2020-08-14 16:06:01 +0630

    second comit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '64ec529a541d150b47a9fb3acd258363'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '273f1fd9-7255-4b69-ba3e-188597049ed4'
+};
